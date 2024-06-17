b13ebc73554a225023f7afa0ac276f83d4c09b9c
Anand Verma <anandverma749@gmail.com>
2024-06-17 09:33:27 +0530

    All done.

--- index.js
+++ index.js
@@ -1,94 +1,91 @@
 let display = document.getElementById("display-area");
-// let poweron = true;
 let operands = document.getElementsByClassName("operands");
+let poweron = document.getElementById("power-on");
+let poweroff = document.getElementById("power-off");
+let displayareaoff = document.getElementById("display-area-off");
+let equal = document.getElementById("equal");
+let equal1 = document.getElementById("equal1");
+let showhistory = document.getElementById("show-history");
+let hidehistory = document.getElementById("hide-history");
+let hist = document.getElementById("hist");
+let clear = document.getElementById("clear");
+let backspace = document.getElementById("backspace");
+let history = document.getElementById("history-container");
+let lkeys = Object.keys(localStorage);
+let body = document.getElementById("body");
 
 for (let i = 0; i < operands.length; i++) {
   operands[i].addEventListener("click", function () {
     display.innerText += this.innerText;
-    console.log(this.innerText);
   });
 }
 
-document.getElementById("power-on").addEventListener("click", function () {
-  document.getElementById("power-on").classList.toggle("hide");
-  document.getElementById("power-off").classList.toggle("hide");
-  document.getElementById("display-area-off").classList.toggle("hide");
-  document.getElementById("display-area").classList.toggle("hide");
-  document.getElementById("equal").classList.add("hide");
-  document.getElementById("equal1").classList.remove("hide");
-});
-document.getElementById("power-off").addEventListener("click", function () {
-    document.getElementById("power-off").classList.toggle("hide");
-    document.getElementById("power-on").classList.toggle("hide");
-    document.getElementById("display-area-off").classList.toggle("hide");
-    document.getElementById("display-area").classList.toggle("hide");
-    document.getElementById("equal").classList.remove("hide");
-    document.getElementById("equal1").classList.add("hide");
-    display.innerText = "";
-  // display.innerText = "0";
+poweron.addEventListener("click", function () {
+  poweron.classList.toggle("hide");
+  poweroff.classList.toggle("hide");
+  displayareaoff.classList.toggle("hide");
+  display.classList.toggle("hide");
+  equal.classList.add("hide");
+  equal1.classList.remove("hide");
+  body.classList.remove("background");
 });
 
-document.getElementById("show-history").addEventListener("click", function () {
-  document.getElementById("hide-history").classList.remove("hide");
-  document.getElementById("show-history").classList.add("hide");
+poweroff.addEventListener("click", function () {
+  poweroff.classList.toggle("hide");
+  poweron.classList.toggle("hide");
+  displayareaoff.classList.toggle("hide");
+  display.classList.toggle("hide");
+  equal.classList.remove("hide");
+  equal1.classList.add("hide");
+  display.innerText = "";
+  body.classList.add("background");
+});
+
+showhistory.addEventListener("click", function () {
+  hidehistory.classList.remove("hide");
+  showhistory.classList.add("hide");
   history.classList.add("hide");
-  document.getElementById("hist").classList.add("hide");
-});
-document.getElementById("hide-history").addEventListener("click", function () {
-    document.getElementById("show-history").classList.remove("hide");
-    document.getElementById("hide-history").classList.add("hide");
-    history.classList.remove("hide");
-    document.getElementById("hist").classList.remove("hide");
+  hist.classList.add("hide");
 });
 
-document.getElementById("clear").addEventListener("click", function () {
-  console.log("clear button clicked.");
+hidehistory.addEventListener("click", function () {
+  showhistory.classList.remove("hide");
+  hidehistory.classList.add("hide");
+  history.classList.remove("hide");
+  hist.classList.remove("hide");
+});
+
+clear.addEventListener("click", function () {
   display.innerText = "";
 });
 
-document.getElementById("backspace").addEventListener("click", function () {
-  console.log("backspace button clicked");
+backspace.addEventListener("click", function () {
   let a = display.innerText.slice(0, -1);
   display.innerText = a;
 });
 
-document.getElementById("equal").addEventListener("click", function () {
-  console.log("equal");
-  console.log(display.innerText);
-  console.log(eval(display.innerText));
+equal.addEventListener("click", function () {
+  let operation = display.innerText;
   display.innerText = eval(display.innerText);
   let key = Date.now();
-  localStorage.setItem(key, display.innerText);
+  localStorage.setItem(key, operation + " = " + display.innerText);
   let temp = document.createElement("div");
-    // let temp = `<div>${JSON.parse(localStorage.getItem(lkeys[i]))}</div>`;
-    // temp.innertext = `<div>${localStorage.getItem(lkeys[i])}</div>`;
-    temp.innerHTML = `<div>${localStorage.getItem(key)}</div>`;
-    // console.log(temp);
-    history.insertBefore(temp, history.firstChild);
-    // history.appendChild(temp);
+  temp.innerHTML = `<div>${localStorage.getItem(key)}</div>`;
+  history.insertBefore(temp, history.firstChild);
 });
 
-let lkeys = Object.keys(localStorage);
-
-// console.log(lkeys);
-
-lkeys.sort(function (a, b) {
+lkeys
+  .sort(function (a, b) {
     return a - b;
-  }).reverse();
-
-let history = document.getElementById("history-container");
+  })
+  .reverse();
 
 if (localStorage.length) {
   for (let i = 0; i < localStorage.length; i++) {
     let temp = document.createElement("div");
-    // let temp = `<div>${JSON.parse(localStorage.getItem(lkeys[i]))}</div>`;
-    // temp.innertext = `<div>${localStorage.getItem(lkeys[i])}</div>`;
     temp.innerHTML = `<div>${localStorage.getItem(lkeys[i])}</div>`;
-    // console.log(temp);
     history.appendChild(temp);
-    // console.log(temp);
-    // console.log(localStorage.getItem(lkeys[i]));
   }
 } else {
-    history.classList.add("hide");
+  history.classList.add("hide");
 }
